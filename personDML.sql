c54d4e94e52b41847bc0d41faa223d06fa40989e
Aaditya Jaiswal <78849620+AADI-1331@users.noreply.github.com>
2022-04-08 14:55:52 +0530

    Create personDML.sql

---
+++ personDML.sql
@@ -0,0 +1,11 @@
+select * from person
+insert into person values("Aaditya","xyz",2001-01-26,70,6)
+
+insert into person values("RAJ","abc",2002-02-20,50,5)
+insert into person values("rahul","chronic",2001-01-31,69,6)
+
+
+update person set Name="Aditya";
+
+delete from person
+where Name="RAJ";
